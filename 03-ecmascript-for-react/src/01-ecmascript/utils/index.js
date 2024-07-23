ca1693d9ad9a2e5e414d60414a814d48ea91ffe0
heejae <leeheejaelcode@gmail.com>
2024-07-23 15:25:54 +0900

    [학습] ES 모듈
    - 모듈 내보내기
    - 모듈 가져오기
    - 모듈 다시 내보내기
    - 모듈 별칭 이름 설정

---
+++ 03-ecmascript-for-react/src/01-ecmascript/utils/index.js
@@ -0,0 +1,6 @@
+// re-export
+// 다시 내보내기 구분
+
+export { default as numberWithComma } from "./numberWithComma.js";
+export { removeSpaceHTMLString } from "./removeSpaceHTMLString.js";
+export { shuffle } from "./shuffle.js";
