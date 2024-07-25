a7af443fe3638692b4889f76f6d59c24addc7ada
heejae <leeheejaelcode@gmail.com>
2024-07-25 09:41:25 +0900

    [역사] JavaScript 모듈의 역사 - AMD vs Common.JS 진영의 대립 - ES mODULES 표준의 등장 - Node.js 런타임 환경에서 적용하는 방법

---
+++ 03-ecmascript-for-react/server.mjs
@@ -0,0 +1,14 @@
+// mjs
+
+// const liveServer = require("live-server");
+import liveServer from "live-server";
+
+const params = {
+  port: 3000, // Set the server port. Defaults to 8080.
+  host: "localhost", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+  root: ".", // Set root directory that's being served. Defaults to cwd.
+  open: false, // When false, it won't load your browser by default.
+  mount: [["/", "./public"]], // Mount a directory to a route.
+};
+
+liveServer.start(params);
