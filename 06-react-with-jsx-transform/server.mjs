1cc20a72e10365f55a5b8bf76cb119e8ce248a30
heejae <leeheejaelcode@gmail.com>
2024-07-26 15:41:45 +0900

    [학습] JSX 트랜스폼 (with Babel)

--- 06-react-with-jsx-transform/server.mjs
+++ 06-react-with-jsx-transform/server.mjs
@@ -1,12 +1,12 @@
 // JavaScript를 사용해 Node.js 런타임 환경에서 실행시킬 프로그램 작성
 
-import liveServer from 'live-server';
+import liveServer from "live-server";
 
 const params = {
-  host: 'localhost',
+  host: "localhost",
   port: 3000,
   open: false,
-  mount: [['/', './public']],
+  mount: [["/", "./public"]],
 };
 
 liveServer.start(params);
