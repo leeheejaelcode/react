45394c92a56af8e62f928bf7c017e961cc3575ce
heejae <leeheejaelcode@gmail.com>
2024-07-26 11:11:17 +0900

    [학습] NumberList 함수 컴포넌트 작성

--- 05-react-component-with-props/src/main.js
+++ 05-react-component-with-props/src/main.js
@@ -1,24 +1,34 @@
-import React from "https://esm.sh/react";
+import React, { createElement as h } from "https://esm.sh/react";
 import { createRoot } from "https://esm.sh/react-dom";
-
+// 데이터 가져오기
+import listData from "./data/list.js";
 // 컴포넌트 불러오기
-import NumberList from "./components/NumberList.class.js";
+import NumberList from "./components/NumberList.function.js";
 import ArchitectureList from "./components/architectures/ArchitectureList.class.js";
 import ArchitectureItem from "./components/architectures/ArchitectureItem.class.js";
 
-// 데이터 가져오기
-import listData from "./data/list.js";
+// 리액트 앱을 렌더링 할 DOM 요소 참조
 const container = document.getElementById("react-app");
 
+// DOM 요소가 존재한다면?
 if (container) {
-  const architectureList = React.createElement(ArchitectureList, {
+  // ArchitectureList 컴포넌트 -> 리액트 엘리먼트 생성
+  // ArchitectureList 컴포넌트에 속성(props) 전달
+  const architectureList = h(ArchitectureList, {
     lang: "en",
     children: listData.items.map(({ id, title }) =>
-      React.createElement(ArchitectureItem, { id, title })
+      h(ArchitectureItem, { id, title })
     ),
   });
 
-  createRoot(container).render(architectureList);
-} else {
-  alert('문서에 "#app" 요소가 존재하지 않습니다.');
+  // NumberList 컴포넌트 -> 리액트 엘리먼트 생성
+  // NumberList 컴포넌트에 속성(props) 전달
+  const numberList = h(NumberList, { count: 9 });
+  // DOM 요소를 리액트 돔 루트로 만든 후, 리액트 앱 렌더링
+  createRoot(container).render(numberList);
+}
+// 존재하지 않는다면?
+else {
+  // 개발자에게 경고
+  console.warn('문서에 "#app" 요소가 존재하지 않습니다.');
 }
