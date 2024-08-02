efe7075f8ac9731dfe48ecdbea7e60f4d75e7a9c
heejae <leeheejaelcode@gmail.com>
2024-08-02 10:40:40 +0900

    [학습] 리액트 관점에서의 순수와 부수 효과

--- 09-component-purity/src/learn/about-purity.js
+++ 09-component-purity/src/learn/about-purity.js
@@ -4,3 +4,16 @@
 // - [ ] 함수의 순수성(purity)이란?
 // - [ ] 함수가 순수한 지 여부는 어떻게 확인해야 할까?
 // --------------------------------------------------------------------------
+/**@type{(text:string, limit?:number) => string} */
+function truncateText(text, limit = 100) {
+  if (text.length > limit) {
+    return text.slice(0, limit) + '...';
+  }
+  return text;
+}
+
+// 순수 함수의 요건 : 동일 입력 -> 동일 출력
+// 순수함이란? 계산된 결과가 동일함을 말한다.
+const testText = 'yamoo9 '.repeat(35);
+console.log(1, truncateText(testText));
+console.log(2, truncateText(testText));
--- 09-component-purity/src/main.jsx
+++ 09-component-purity/src/main.jsx
@@ -2,7 +2,8 @@ import '@/styles/main.css';
 // import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './learn/about-purity.js';
+// import './learn/about-purity.js';
+import './learn/about-side-effects';
 const container = document.getElementById('react-app');
 
 if (!container) throw new Error('문서에 "#app" 요소가 존재하지 않습니다.');
