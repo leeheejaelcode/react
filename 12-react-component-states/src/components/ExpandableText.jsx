93f0a0f8431df85c5dcce89fa702bd9f424e255a
heejae <leeheejaelcode@gmail.com>
2024-08-05 15:22:49 +0900

    [학습] 확장 가능한 텍스트 컴포넌트 선언적으로 제어
    - 함수 컴포넌트 상태 제어
    - 마크업을 변경 (컴포넌트 다시 실행)
    - DOM 커밋(변경된 부분만 반영)

--- 12-react-component-states/src/components/ExpandableText.jsx
+++ 12-react-component-states/src/components/ExpandableText.jsx
@@ -7,9 +7,43 @@
 // - [ ] `limit` 속성 값보다 `text` 길이가 긴 경우 확장 or 축소 버튼 표시
 // - [ ] 확장 or 축소 버튼을 사용자가 클릭하면 텍스트 확장 또는 축소되어 표시
 // --------------------------------------------------------------------------
+import { string, number } from 'prop-types';
+import './ExpandableText.css';
+import { useState } from 'react';
+ExpandableText.propTypes = {
+  children: string.isRequired,
+  limit: number,
+};
 
-function ExpandableText() {
-  return <></>;
+function ExpandableText({ children, limit = 250 }) {
+  const [isExpand, setIsExpand] = useState(false);
+
+  // 파생된 상태
+  const isExpandable = children.length > limit;
+  let rednerText = children;
+  if (isExpandable) {
+    rednerText = children.slice(0, limit) + '...';
+  }
+
+  const buttonLabel = isExpandable ? '확장' : '축소';
+
+  const handleExpand = () => {
+    // 상태 업데이트
+    const nextExpandValue = !isExpand;
+    console.log('상태 업데이트');
+    setIsExpand(!nextExpandValue);
+  };
+
+  return (
+    <div className="ExpandableText">
+      <p>{isExpand ? children : rednerText}</p>
+      {isExpandable && (
+        <button type="button" onClick={handleExpand}>
+          {buttonLabel}
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default ExpandableText;
