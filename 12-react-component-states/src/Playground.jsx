6d4611a50920c996f5a4545ce6f5221376d1bc46
heejae <leeheejaelcode@gmail.com>
2024-08-05 14:20:02 +0900

    [학습] 이용 약관 선언적으로 제어

---
+++ 12-react-component-states/src/Playground.jsx
@@ -0,0 +1,10 @@
+import TermAndConditions from '@/components/TermAndCondition';
+function Playground() {
+  return (
+    <>
+      <TermAndConditions />
+    </>
+  );
+}
+
+export default Playground;
