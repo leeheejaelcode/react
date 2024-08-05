c93eb537ca5e0ff5f566af3486d439b8ecef621d
heejae <leeheejaelcode@gmail.com>
2024-08-05 11:29:03 +0900

    [학습] 선언적 프로그래밍 (WHAT) III
    - 리액트와의 유사성
    - 리액트의 렌더링 프로세스
      1. 상태 선언
      2. 선언된 상태 변경
      3. 화면 업데이트

--- 11-declarative-programming/src/lib/createState.ts
+++ 11-declarative-programming/src/lib/createState.ts
@@ -3,7 +3,8 @@ interface State {
 }
 
 type StateAction = (key: string, value: any) => void;
-
+// 선언된 상태 관리 미니 툴
+// const [state(object), setState(function)] = createState(data, render)
 const createState = (
   data: State,
   callback: () => void
@@ -16,7 +17,7 @@ const createState = (
     },
     set(target, prop, newValue) {
       if (!allowUpdate) {
-        console.warn('🚫 스토어 데이터를 직접 수정할 수 없습니다.');
+        console.warn("🚫 스토어 데이터를 직접 수정할 수 없습니다.");
         return false;
       }
 
