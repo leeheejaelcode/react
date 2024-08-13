934aff64e9ce9d58d30240ed1765e938f04c6aeb
heejae <leeheejaelcode@gmail.com>
2024-08-13 15:20:41 +0900

    [학습] 인뷰 애니메이션 II
    - 스크롤 트리거 애니메이션
    - 뷰포트 영역 밖으로 나갔을 때 감지, 처리

--- 14-using-react-refs/src/routes/motion-one-in-view/components/ScrollTriggerItem.jsx
+++ 14-using-react-refs/src/routes/motion-one-in-view/components/ScrollTriggerItem.jsx
@@ -1,7 +1,7 @@
 import { exact, number, string } from 'prop-types';
 import S from './ScrollTriggerItem.module.css';
 import { useRef } from 'react';
-import { animate, inView } from 'motion';
+import { animate, inView, timeline } from 'motion';
 ScrollTriggerItem.propTypes = {
   item: exact({
     id: number.isRequired,
@@ -17,20 +17,36 @@ function ScrollTriggerItem({ item }) {
 
     if (el)
       inView(el, ({ target }) => {
-        animate(
-          target,
-          { opacity: 1 },
-          {
-            duration: 1,
-          }
-        );
-        animate(
-          p,
-          { x: [-500, 0] },
-          {
-            duration: 1,
-          }
-        );
+        // animate(
+        //   target,
+        //   { opacity: 1 },
+        //   {
+        //     duration: 1,
+        //   }
+        // );
+        // animate(
+        //   p,
+        //   { x: [-500, 0] },
+        //   {
+        //     duration: 1,
+        //   }
+        // );
+        // 타임라인 애니메이션
+
+        const sequence = [
+          [target, { opacity: [0, 1] }, { duration: 0.6 }],
+          [p, { opacity: [0, 1], y: [20, 0] }, { duration: 0.6 }],
+        ];
+        const options = {
+          easing: 'ease-in-out',
+        };
+        const animation = timeline(sequence, options);
+        // inView()함수에 설정된 콜백 함수가 반환하는 함수는
+        // 엘리먼트가 뷰포트를 벗어났을 때 실행함
+
+        return () => {
+          animation.stop();
+        };
       });
   };
 
