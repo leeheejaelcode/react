8fb6a679af68094a86b55ea5d20563487c1fe3fb
heejae <leeheejaelcode@gmail.com>
2024-08-13 15:03:01 +0900

    [학습] 인뷰 애니메이션 I
    - inView() 함수 활용
    - 스크롤 트리거 애니메이션

--- 14-using-react-refs/src/routes/motion-one-in-view/components/ScrollTriggerItem.jsx
+++ 14-using-react-refs/src/routes/motion-one-in-view/components/ScrollTriggerItem.jsx
@@ -1,6 +1,7 @@
 import { exact, number, string } from 'prop-types';
 import S from './ScrollTriggerItem.module.css';
-
+import { useRef } from 'react';
+import { animate, inView } from 'motion';
 ScrollTriggerItem.propTypes = {
   item: exact({
     id: number.isRequired,
@@ -10,12 +11,38 @@ ScrollTriggerItem.propTypes = {
 };
 
 function ScrollTriggerItem({ item }) {
+  const pRef = useRef(null);
+  const setScrollTrigger = (el) => {
+    const p = el.querySelector('p');
+
+    if (el)
+      inView(el, ({ target }) => {
+        animate(
+          target,
+          { opacity: 1 },
+          {
+            duration: 1,
+          }
+        );
+        animate(
+          p,
+          { x: [-500, 0] },
+          {
+            duration: 1,
+          }
+        );
+      });
+  };
+
   return (
     <article
+      ref={setScrollTrigger}
       className={S.component}
       style={{ background: `url(${item.image}) no-repeat center / cover` }}
     >
-      <p className={S.text}>{item.text}</p>
+      <p className={S.text} ref={pRef}>
+        {item.text}
+      </p>
     </article>
   );
 }
