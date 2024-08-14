376cdc9d35b162c0580ee74bde23e89c63ddcc4a
heejae <leeheejaelcode@gmail.com>
2024-08-14 14:44:09 +0900

    [학습] 이펙트 활용 (문서 제목 동기화)
    - 리액트 외부 시스템과 동기화
    - 브라우저의 문서 제목 동기화 (반응성 데이터 변경)
    - 이펙트 추가 (useEffect 훅)
    - 이펙트 콜백 함수 설정
    - 종속성(의존성) 설정 (배열에 반응성 데이터 추가)

--- 15-effect-sync-external-system/src/routes/sync-document-title/components/Counter.jsx
+++ 15-effect-sync-external-system/src/routes/sync-document-title/components/Counter.jsx
@@ -5,26 +5,75 @@
 // - [ ] step 값이 변경될 때에는 불필요한 문서 제목 동기화가 되지 않도록 설정합니다.
 // --------------------------------------------------------------------------
 
-import { useId, useState } from 'react';
+import { useId, useState, useEffect } from 'react';
 import S from './Counter.module.css';
+// 컴포넌트 jsx가 실제 DOM 엘리먼트로 마운트 되었나?
+
+const DOCUMENT_TITLE = document.title;
 
 function Counter() {
+  // useEffect (effectCallback,[dependencies])이펙트
+  // 첫번째 인수 = 이펙트 콜백 함수
+  // - DOM 커밋 이후 시점에 실행
+  // - 상태가 업데이트 될 때마다 실행
+  // 두번째 인수 = 종속성(의존성)배열
+  // - 아예 설정되지 않은 경우(undefined), 매번 실행된다. 렌더링시마다 매번 실행된다)
+  // - 배열이 비어있는 경우, 마운트 시점에서 1회 실행된다.
+
+  useEffect(() => {
+    // 이 안에서 리액트 엘리먼트로 작성한 요소에 접근이 가능할까?
+    const increaseButton = document.querySelector("[aria-label='카운트 증가']");
+    const decreaseButton = document.querySelector("[aria-label='카운트 감소']");
+    increaseButton.setAttribute(
+      'title',
+      increaseButton.getAttribute('aria-label')
+    );
+
+    decreaseButton.setAttribute(
+      'title',
+      decreaseButton.getAttribute('aria-label')
+    );
+  }, []);
+
   const id = useId();
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
+
+  // useEffect(() => {
+  //   // 외부 시스템인 브라우저의 문서 제목을 변경하려 한다.
+  //   document.title = `(${count})` + DOCUMENT_TITLE;
+  // }, [count]);
+
+  // useEffect(() => {
+  //   // 외부 시스템인 브라우저의 문서 제목을 변경하려 한다.
+  //   console.log(`step이 ${step}로 변경됐어요~`);
+  // }, [step]);
+
+  // 반응성 데이터인 상태를 객체로 관리
+  const [state, setState] = useState({
+    count: 0,
+    step: 1,
+  });
+
+  const { count, step } = state;
+
+  useEffect(() => {
+    document.title = `(${count})` + DOCUMENT_TITLE;
+    console.log(`step이 ${step}로 변경됐어요~`);
+  }, [count, step]);
 
   const handleDecrease = () => {
     let nextCount = count - step;
     if (nextCount <= 1) nextCount = 1;
-    setCount(nextCount);
+    setState({ ...state, count: nextCount });
   };
 
   const handleIncrease = () => {
-    setCount(count + step);
+    setState({ ...state, count: count + step });
   };
 
   const handleChangeStep = (e) => {
-    setStep(Number(e.target.value));
+    setState({ ...state, step: Number(e.target.value) });
   };
 
   const isDisabled = count <= 1;
