071418ac4d141f52b9359a8930d8117eb277450d
heejae <leeheejaelcode@gmail.com>
2024-08-14 11:05:48 +0900

    [학습] 채팅 윈도우 구현 II
    - 키다운 이벤트 핸들링
    - Enter 키 누르면 화면 업데이트 요청

--- 14-using-react-refs/src/routes/using-imperative-handle/components/ChatWindow.jsx
+++ 14-using-react-refs/src/routes/using-imperative-handle/components/ChatWindow.jsx
@@ -18,27 +18,55 @@ ChatWindow.propTypes = {
 function ChatWindow({ messages, onAddMessage }) {
   const id = useId();
   const textareaRef = useRef(null);
+  const olRef = useRef(null);
 
   const handleSendMessage = (e) => {
     e.preventDefault();
-
     const formData = new FormData(e.currentTarget);
-
     let newMessage = formData.get('message');
     newMessage = newMessage.trim();
 
+    sendMessage(newMessage);
+  };
+
+  const sendMessage = (newMessage) => {
+    const textarea = textareaRef.current;
+    const ol = olRef.current;
+
     if (newMessage.length <= 0) {
       alert('메시지 내용을 입력하세요!');
-      textareaRef.current.select();
+      textarea.select();
+      return;
     }
+
     onAddMessage?.(newMessage);
+    textarea.value = '';
+
+    // 타이머
+    setTimeout(() => {
+      ol.scrollTo(0, ol.scrollHeight);
+    });
+  };
+
+  const handleKeyDown = (e) => {
+    const { key, shiftKey } = e;
+    if (key == 'Enter' && !shiftKey) {
+      e.preventDefault();
+      const newMessage = e.currentTarget.value.trim();
+      if (newMessage.length > 0) {
+        sendMessage(newMessage);
+      }
+    }
+    if (key == 'Enter' && shiftKey) {
+      console.log('shift+enter');
+    }
   };
 
   return (
     <section className={S.component}>
       <h2 className="sr-only">채팅 화면</h2>
 
-      <ol className={S.chats}>
+      <ol ref={olRef} className={S.chats}>
         {messages.map(({ id, isMe, message }) => {
           const classNames = `${S.chat} ${isMe ? S.me : ''}`.trim();
 
@@ -55,7 +83,12 @@ function ChatWindow({ messages, onAddMessage }) {
           <label htmlFor={id} className="sr-only">
             메시지 입력
           </label>
-          <textarea ref={textareaRef} id={id} name="message" />
+          <textarea
+            ref={textareaRef}
+            id={id}
+            name="message"
+            onKeyDown={handleKeyDown}
+          />
         </div>
         <button type="submit">보내기</button>
       </form>
