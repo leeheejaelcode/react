a57f756dc936896f1a9d2a465d9569dececaf2ad
heejae <leeheejaelcode@gmail.com>
2024-08-16 11:33:36 +0900

    [학습] 이펙트 동기화 / 정리 V - 상태 변경 → 이펙트 콜백 실행 (종속성이 상태로 설정된 경우) - 만약 무한 루프에 빠질 경우 패닉!! (조건 처리 필요) - 클린업(정리) 매우 중요!!!!!

--- 15-effect-sync-external-system/src/routes/effect-sync-and-cleanup/components/UselessCheckbox.jsx
+++ 15-effect-sync-external-system/src/routes/effect-sync-and-cleanup/components/UselessCheckbox.jsx
@@ -5,14 +5,24 @@
 // - [ ] 컴포넌트가 언마운트 된 이,후 남은 이펙트를 깨끗하게 정리합니다.
 // --------------------------------------------------------------------------
 
-import { useId, useState } from 'react';
+import { useId, useState, useEffect } from 'react';
 import S from './UselessCheckbox.module.css';
 
 function UselessCheckbox() {
   const id = useId();
-  const [checked, setChecked] = useState(true);
+  const [checked, setChecked] = useState(false);
 
   const handleChecked = () => setChecked((c) => !c);
+
+  useEffect(() => {
+    if (!checked) {
+      const id = setTimeout(() => {
+        setChecked(true);
+        console.log('쓰잘다꾸없는');
+      }, 1000);
+      return () => clearTimeout(id);
+    }
+  }, [checked]);
 
   return (
     <div className={S.component}>
