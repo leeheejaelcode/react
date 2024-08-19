2596b89114f89b3f5b2dca2b85f5eba08e34a2ad
heejae <leeheejaelcode@gmail.com>
2024-08-19 13:20:36 +0900

    [학습] 싱글 페이지 앱 with React Router 3 - createRoutesFromElements() 함수 활용 - 레가시 리액트 라우터 API 활용

--- 16-using-custom-hooks/src/router.jsx
+++ 16-using-custom-hooks/src/router.jsx
@@ -1,8 +1,14 @@
-import { createBrowserRouter } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+} from 'react-router-dom';
 import Home from '@/page/Home';
 import NoteList from '@/page/NoteList';
 import NewNote from '@/page/NewNote';
 import NoteDetail from '@/page/NoteDetail';
+
+// 최신 문법
 const routes = [
   // routes
   // {path?: string, element?: React.node | null }
@@ -23,6 +29,17 @@ const routes = [
     element: <NoteDetail />,
   },
 ];
-const router = createBrowserRouter(routes);
+// const router = createBrowserRouter(routes);
+
+// 레가시 코드
+const routersFromElement = createRoutesFromElements(
+  <>
+    <Route path="/" element={<Home />} />
+    <Route path="/notes" element={<NoteList />} />
+    <Route path="/notes/new" element={<NewNote />} />
+    <Route path="/notes/detail" element={<NoteDetail />} />
+  </>
+);
+const router = createBrowserRouter(routersFromElement);
 
 export default router;
