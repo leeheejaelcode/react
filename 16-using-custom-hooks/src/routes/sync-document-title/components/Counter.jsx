9c056761090af089958e15eed4330e27130d5294
heejae <leeheejaelcode@gmail.com>
2024-08-20 15:24:17 +0900

    [학습] 커스텀 훅
    - useWebStorage.js
    - 자동 저장(auto save) 기능 추가
    - useLocalStorage() 훅
    - useSessionStorage() 훅

--- 16-using-custom-hooks/src/routes/sync-document-title/components/Counter.jsx
+++ 16-using-custom-hooks/src/routes/sync-document-title/components/Counter.jsx
@@ -1,31 +1,34 @@
-import { useId, useState } from 'react';
-import S from './Counter.module.css';
-import { getStorageData, setStorageData } from '@/utils';
 import useDocumentTitle from '@/hooks/useDocumentTitle';
+import { useLocalStorage, useSessionStorage } from '@/hooks/useWebStorage';
+import { useId } from 'react';
+import S from './Counter.module.css';
 
-const DOCUMENT_INITIAL_TITLE = '문서 제목 동기화';
-const COUNTER_STEP = '@counter/step';
 const COUNTER_COUNT = '@counter/count';
+const COUNTER_STEP = '@counter/step';
 
 function Counter() {
   const id = useId();
 
-  const [count, setCount] = useState(() =>
-    getStorageData(COUNTER_COUNT, 0, 'session')
+  const [count, setCount, countMethods] = useLocalStorage(
+    COUNTER_COUNT,
+    0,
+    true
   );
 
-  // 문서 제목 변경 로직
-  // useEffect(() => {
-  //   document.title = `(${count}) ` + DOCUMENT_INITIAL_TITLE;
-  // }, [count]);
-  // 문서 제목 변경 로직
-  // 파생된 상태 + 커스텀 훅
-  const documentTitle = `(${count}) ` + DOCUMENT_INITIAL_TITLE;
-  useDocumentTitle(documentTitle);
+  const {
+    setItem: setCountItem,
+    deleteItem: deleteCount,
+    allClear,
+  } = countMethods;
 
-  const [step, setStep] = useState(() =>
-    getStorageData(COUNTER_STEP, 1, 'session')
+  useDocumentTitle(`(${count}) 문서 제목 동기화`);
+
+  const [step, setStep, stepControls] = useSessionStorage(
+    COUNTER_STEP,
+    1,
+    true
   );
+  const { setItem: setStepItem, deleteItem: deleteStep } = stepControls;
 
   const handleDecrease = () => {
     let nextCount = count - step;
@@ -42,8 +45,8 @@ function Counter() {
   };
 
   const handleSaveToStorage = () => {
-    setStorageData(COUNTER_COUNT, count, 'session');
-    setStorageData(COUNTER_STEP, step, 'session');
+    setCountItem(count);
+    setStepItem(step);
   };
 
   const isDisabled = count <= 1;
@@ -53,7 +56,16 @@ function Counter() {
       <div className={S.component}>
         <div style={{ marginBlockEnd: 20 }}>
           <button type="button" onClick={handleSaveToStorage}>
-            이벤트로 웹 스토리지 동기화
+            카운트, 스탭 웹 스토리지 저장
+          </button>
+          <button type="button" onClick={deleteCount}>
+            웹 스토리지에 저장된 카운트 삭제
+          </button>
+          <button type="button" onClick={deleteStep}>
+            웹 스토리지에 저장된 스탭 삭제
+          </button>
+          <button type="button" onClick={allClear}>
+            웹 스토리지 데이터 모두 삭제
           </button>
         </div>
 
