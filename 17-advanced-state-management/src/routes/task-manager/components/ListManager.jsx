b47f2b79e2c65b11c367fdea28b71aa9b5951c1d
heejae <leeheejaelcode@gmail.com>
2024-08-22 14:06:49 +0900

    [학습] 리듀서를 사용한 상태 관리 4 - 리스트 상태 관리 - useReducer() 훅 활용

--- 17-advanced-state-management/src/routes/task-manager/components/ListManager.jsx
+++ 17-advanced-state-management/src/routes/task-manager/components/ListManager.jsx
@@ -1,28 +1,61 @@
 /* eslint-disable no-unused-vars */
-import { listReducer } from '@/stores/list';
+import {
+  addItem,
+  editItem,
+  listReducer,
+  deleteItem,
+  filterList,
+  resetList,
+  INITIAL_LIST,
+} from '@/stores/list';
 import { useReducer } from 'react';
 
 export function ListManager() {
-  const [list] = useReducer(listReducer, ['one', 'zero']);
+  const [list, dispatch] = useReducer(listReducer, INITIAL_LIST);
 
   // 추가
-  const handleAddItem = () => {};
-
+  const handleAddItem = () => {
+    dispatch(addItem(crypto.randomUUID().split('-').at(0)));
+  };
   // 수정
-  const handleEditItem = () => {};
-
+  const handleEditItem = (editId) => () => {
+    dispatch(editItem(editId, crypto.randomUUID().split('-').at(0)));
+  };
   // 삭제
-  const handleRemoveItem = () => {};
+  const handleDeleteItem = (deleteId) => () => {
+    dispatch(deleteItem(deleteId));
+  };
 
   // 필터링
-  const handleFilterItems = () => {};
+  const handleFilterItems = () =>
+    dispatch(filterList(Math.floor(Math.random() * 10)));
 
+  const handleResetItems = () => dispatch(resetList());
   return (
     <div>
       <h2 className="headline2">배열관리자</h2>
+      <div role="group">
+        <button type="button" onClick={handleAddItem}>
+          추가
+        </button>
+        <button type="button" onClick={handleFilterItems}>
+          필터링
+        </button>
+        <button type="button" onClick={handleResetItems}>
+          초기화
+        </button>
+      </div>
       <ul>
         {list.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>
+            {item}{' '}
+            <button type="button" onClick={handleEditItem(index)}>
+              수정
+            </button>{' '}
+            <button type="button" onClick={handleDeleteItem(index)}>
+              삭제
+            </button>
+          </li>
         ))}
       </ul>
     </div>
--- 17-advanced-state-management/src/stores/list.js
+++ 17-advanced-state-management/src/stores/list.js
@@ -1,1 +1,69 @@
-export const listReducer = (state) => state;
+const ACTION_TYPE = {
+  ADD: '추가',
+  EDIT: '수정',
+  DELETE: '삭제',
+  FILTER: '필터링',
+  RESET: '초기화',
+};
+
+const { ADD, EDIT, DELETE, FILTER, RESET } = ACTION_TYPE;
+
+export const addItem = (newItem) => ({
+  type: ADD,
+  payload: newItem,
+});
+
+export const editItem = (editId, nextItem) => ({
+  type: EDIT,
+  payload: { editId, nextItem },
+});
+export const deleteItem = (deleteId) => ({
+  type: DELETE,
+  payload: { deleteId },
+});
+export const filterList = (filterKey) => ({
+  type: FILTER,
+  payload: { filterKey },
+});
+
+export const resetList = () => ({
+  type: RESET,
+});
+
+const generateItem = () => crypto.randomUUID().split('-').at(0);
+
+export const INITIAL_LIST = Array(9)
+  .fill(null)
+  .map(() => generateItem());
+
+export const listReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ADD: {
+      const nextState = [...state, payload];
+      return nextState;
+    }
+    case EDIT: {
+      const { editId, nextItem } = payload;
+      const nextState = state.map((item, index) => {
+        return index !== editId ? item : nextItem;
+      });
+      return nextState;
+    }
+    case DELETE: {
+      const { deleteId } = payload;
+      const nextState = state.filter((_, index) => index !== deleteId);
+      return nextState;
+    }
+    case FILTER: {
+      const filterKey = payload.filterKey.toString();
+      const nextState = state.filter((item) => item.includes(filterKey));
+      return nextState;
+    }
+    case RESET: {
+      return [...INITIAL_LIST];
+    }
+    default:
+      console.log('기본 값');
+      return state;
+  }
+};
